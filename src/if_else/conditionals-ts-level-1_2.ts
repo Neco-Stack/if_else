1d4c187b125c2490ba1ac8fdbd97ba53b2345dda
Neco_Stack <necoabay@googlemail.com>
2024-10-01 21:18:41 +0200

    conditionals

---
+++ src/if_else/conditionals-ts-level-1_2.ts
@@ -0,0 +1,13 @@
+const weatherInput = document.querySelector("#weatherInput") as HTMLInputElement;
+const sendenBtn = document.querySelector("#sendenInput") as HTMLElement;
+const result = document.querySelector("#result") as HTMLElement;
+
+if(weatherInput){
+    const inputValue = weatherInput.value;
+    console.log(inputValue);
+}
+
+
+
+
+
